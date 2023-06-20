a167e0e722b01ece62ad472379b6ed800d648139
BrownofDarkness <takoubrown@gmail.com>
2023-06-20 21:06:47 +0100

    update admin

--- src/AnnoncesApp/core/admin.py
+++ src/AnnoncesApp/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Voiture, Annonce, Photo
 
 # Register your models here.
+
+
+@admin.register(Voiture)
+class VoitureAdmin(admin.ModelAdmin):
+    list_display = ("id", "model", "marque")
+    search_fields = ("model","marque")
+    
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("id", "image", "photo_preview")
+
+    readonly_fields = ["photo_preview"]
+    
+@admin.register(Annonce)
+class AnnonceAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "status", "montant")
+    search_fields = ("title", "status")
--- src/AnnoncesApp/core/models.py
+++ src/AnnoncesApp/core/models.py
@@ -1,3 +1,4 @@
+from django.utils.html import format_html
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -12,7 +13,7 @@ class Voiture(models.Model):
 class Annonce(models.Model):
     STATUTS = (("en attente", "en attente"), ("validé", "validé"), ("retiré","retiré"),)
     
-    titre = models.CharField()
+    title = models.CharField(max_length=255)
     description = models.TextField()
     status = models.CharField( max_length=255, choices= STATUTS, default=STATUTS[0])
     montant = models.IntegerField()
@@ -20,5 +21,11 @@ class Annonce(models.Model):
     propriétaire = models.ForeignKey(User, on_delete = models.CASCADE, related_name="annonces")
 
 class Photo(models.Model):
-    iamge = models.ImageField(upload_to='/voitures')
+    image = models.ImageField(upload_to='voitures/')
     voiture = models.ForeignKey(Voiture, on_delete = models.CASCADE, related_name='images')
+    
+    def photo_preview(self):
+        if self.image:
+            return format_html(f"<img src='{self.photo.url}' width='400px' heigth='400px' class='rounded float-right' />")
+        
+        return None
