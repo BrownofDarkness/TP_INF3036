e91a62ef027dc2d41c0fc0886f71e557052e750a
Tomdieu <ivantomdio@gmail.com>
2023-06-21 10:05:00 +0100

    fixed

--- src/AnnoncesApp/AnnoncesApp/urls.py
+++ src/AnnoncesApp/AnnoncesApp/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("Core.urls"))]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
